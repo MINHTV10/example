9de1c9fc72a0432050951e339587fa86d7fbdb89
MINHTV10 <minhhauik9@gmail.com>
2023-02-08 22:56:45 +0700

    example non sync

---
+++ Example Multithread.java
@@ -0,0 +1,66 @@
+Example Multithread
+
+class Table {
+    void printTable(int n) {// method không synchronized
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(n * i);
+            try {
+                Thread.sleep(400);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+ 
+    }
+}
+ 
+class MyThread1 extends Thread {
+    Table t;
+ 
+    MyThread1(Table t) {
+        this.t = t;
+    }
+ 
+    public void run() {
+        t.printTable(5);
+    }
+}
+ 
+class MyThread2 extends Thread {
+    Table t;
+ 
+    MyThread2(Table t) {
+        this.t = t;
+    }
+ 
+    public void run() {
+        t.printTable(100);
+    }
+}
+ 
+public class TestSynchronization1 {
+    public static void main(String args[]) {
+        Table obj = new Table();// tao mot object
+        MyThread1 t1 = new MyThread1(obj);
+        MyThread2 t2 = new MyThread2(obj);
+        t1.start();
+        t2.start();
+    }
+}
+
+
+/*
+output
+
+5
+100
+200
+10
+300
+15
+400
+20
+25
+500
+
+*/
